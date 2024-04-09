e65fa41ee82d04da55e84ea0e62df876df46d1a5
beatrizac03 <beatrizaguiarc14@gmail.com>
2024-04-09 16:25:53 -0300

    updt 1.3

--- script.js
+++ script.js
@@ -2,7 +2,30 @@ let mainImgSelected = document.querySelector('.p-mainimg')
 let containerImgs = document.querySelector('.p-secimgs')
 let eachSecImg = document.querySelectorAll('.p-eachsecimg')
 let qntityItemsField = document.querySelector('.number-qtity')
+let btnAddQntity = document.querySelector('.sign-add')
+let btnRemoveQntity = document.querySelector('.sign-remove')
 
+
+let numbQntity = 0
+qntityItemsField.innerHTML = numbQntity
+qntityItemsField.style.display = 'flex'
+qntityItemsField.style.alignItems = 'center'
+qntityItemsField.style.justifyContent = 'center'
+qntityItemsField.style.fontWeight = '500'
+qntityItemsField.style.fontSize = 'var(--small)'
+
+
+function addQntity () {
+    numbQntity++
+    qntityItemsField.innerHTML = numbQntity
+}
+
+function removeQntity () {
+    if (numbQntity > 0) {
+        numbQntity--
+        qntityItemsField.innerHTML= numbQntity
+    } 
+}
 
 function reduceOpacity (event) {
     let imgEvent = event.target 
@@ -12,20 +35,24 @@ function reduceOpacity (event) {
 
 function increaseOpacity (event) {
     event.target.style.opacity = '100%'
-    console.log('naaaaaaaao')
 }
 
 function switchMainImg (event) {
     let divImg = event.target
-    console.log(divImg)
+    // console.log(divImg)
 
     let style = window.getComputedStyle(divImg)
 
     let bgImgURL = style.getPropertyValue("background-image")
-    console.log(bgImgURL)
+    // console.log(bgImgURL)
 
     mainImgSelected.style.backgroundImage = `${bgImgURL}`
 }  
+
+
+btnAddQntity.addEventListener('click', addQntity)
+btnRemoveQntity.addEventListener('click', removeQntity)
+
 
 eachSecImg.forEach( img => {
     img.addEventListener('mouseenter', reduceOpacity)
@@ -33,12 +60,6 @@ eachSecImg.forEach( img => {
     img.addEventListener('click', switchMainImg)
 })
 
-
-mainImgSelected.addEventListener('click', function(event) {
-    // console.log('simmmm')
-    // console.log(event.target)
-    // event.target.style.backgroundImage = 'url(assets/images/icon-plus.svg)'
-})
 
 
 
